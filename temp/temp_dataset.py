f4f1a194fe139a991de9d45b099f8742063f713d
alechandrakanth <99846003+alechandrakanth@users.noreply.github.com>
2022-12-14 11:24:47 +0100

    temp
    uploading code for temp dataset retrieval and validate fever condition

---
+++ temp/temp_dataset.py
@@ -0,0 +1,12 @@
+import csv
+import random
+
+with open("./IOT-temp.csv", 'r') as file:
+    csvreader = csv.reader(file)
+    temp = random.choice([row[0] for row in csvreader])
+    temp_int = int(temp)
+    print(temp_int, type(temp_int))
+    if temp_int > 36:
+        print("you have fever, pls go to doctor")
+    else:
+        print("pls proceed to face mask detector")
