3f4a909040eefdfce0b23fb2ee2d5bb4a65796e2
Tanmay Pradhan <tanmaypradhan1990@gmail.com>
2023-01-01 18:54:58 +0100

    TensorLite model and implementation

--- app/src/main/java/com/example/iotcamera/MainActivity.java
+++ app/src/main/java/com/example/iotcamera/MainActivity.java
@@ -1,12 +1,30 @@
 package com.example.iotcamera;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.iotcamera.ml.MaskDetector;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +33,15 @@ public class MainActivity extends AppCompatActivity {
     // Define the button and imageview type variable
     Button camera_open_id;
     ImageView click_image_id;
+    TextView result;
+    TextView prediction;
 
+
+    int imageSize=224;
+
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,25 +50,94 @@ public class MainActivity extends AppCompatActivity {
         // By ID we can get each component which id is assigned in XML file get Buttons and imageview.
         camera_open_id = findViewById(R.id.camera_button);
         click_image_id = findViewById(R.id.click_image);
+        result= findViewById(R.id.result);
+        prediction=findViewById(R.id.prediction);
 
         // Camera_open button is for open the camera and add the setOnClickListener in this button
-        camera_open_id.setOnClickListener(v -> {
-            // Create the camera_intent ACTION_IMAGE_CAPTURE it will open the camera for capture the image
-            Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            // Start the activity with camera_intent, and request pic id
-            startActivityForResult(camera_intent, pic_id);
+        camera_open_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                    Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(camera_intent,3);
+                }
+                else{
+                    requestPermissions(new String[]{Manifest.permission.CAMERA},100);
+                }
+            }
         });
+
+
     }
 
-    // This method will help to retrieve the image
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        // Match the request 'pic id with requestCode
-        if (requestCode == pic_id) {
-            // BitMap is data structure of image file which store the image in memory
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            // Set the image in imageview for display
-            click_image_id.setImageBitmap(photo);
+    public void classifyImage(Bitmap image){
+        try {
+            MaskDetector model = MaskDetector.newInstance(getApplicationContext());
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer=ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+
+            Log.d("shape", byteBuffer.toString());
+            Log.d("shape", inputFeature0.getBuffer().toString());
+
+            int[] intValues= new int[imageSize*imageSize];
+            image.getPixels(intValues,0,image.getWidth(),0,0,image.getWidth(),image.getHeight());
+            int pixel=0;
+            for(int i=0;i<imageSize;i++){
+                for(int j=0;j<imageSize;j++){
+                    int val=intValues[pixel++];
+                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 255));
+                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 255));
+                    byteBuffer.putFloat((val >> 0xFF) * (1.f / 255));
+
+                }
+            }
+
+
+            inputFeature0.loadBuffer(byteBuffer);
+
+            // Runs model inference and gets result.
+            MaskDetector.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+            float[] predictions =outputFeature0.getFloatArray();
+
+            int maxPos=0;
+            float maxPrediction=0;
+            for(int i=0;i<predictions.length;i++){
+                if(predictions[i]>maxPrediction){
+                    maxPrediction=predictions[i];
+                    maxPos=i;
+                }
+            }
+            String[] labels={"Mask","No Mask"};
+            result.setText(labels[maxPos]+" detected");
+            prediction.setText(String.format("%2f", maxPrediction*100f));
+
+
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+            System.out.println("Classify image failed:" +e.getMessage());
         }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 3) {
+                Bitmap image = (Bitmap) data.getExtras().get("data");
+                int dimension = Math.min(image.getWidth(), image.getHeight());
+                image = ThumbnailUtils.extractThumbnail(image, dimension, dimension);
+                click_image_id.setImageBitmap(image);
+                image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+                classifyImage(image);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+
     }
 }
