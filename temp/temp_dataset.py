c1aa1d232c67fa93d254d95d5856e40fd0f3aa19
alechandrakanth <99846003+alechandrakanth@users.noreply.github.com>
2023-01-05 19:52:17 +0100

    Update print

--- temp/temp_dataset.py
+++ temp/temp_dataset.py
@@ -5,8 +5,8 @@ with open("./IOT-temp.csv", 'r') as file:
     csvreader = csv.reader(file)
     temp = random.choice([row[0] for row in csvreader])
     temp_int = int(temp)
-    print(temp_int, type(temp_int))
-    if temp_int > 36:
-        print("you have fever, pls go to doctor")
-    else:
-        print("pls proceed to face mask detector")
+    print(temp_int)
+    #if temp_int > 36:
+    #    print("you have fever, pls go to doctor")
+    #else:
+    #    print("pls proceed to face mask detector")
